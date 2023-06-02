33584bbeeab1fc008fbbb8c161bbf213b66e5bd9
Andrey <rakdav@inbox.ru>
2023-06-02 11:34:57 +0200

    Retrofit2

--- app/src/main/java/com/example/myapplication/Api/APIClient.java
+++ app/src/main/java/com/example/myapplication/Api/APIClient.java
@@ -1,28 +1,9 @@
 package com.example.myapplication.Api;
 
-import okhttp3.OkHttpClient;
-import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
 
 public class APIClient
 {
     private static Retrofit retrofit = null;
-    static Retrofit getClient() {
 
-        HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
-        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
-        OkHttpClient client = new OkHttpClient.Builder().addInterceptor(interceptor).build();
-
-
-        retrofit = new Retrofit.Builder()
-                .baseUrl("https://api.openweathermap.org/data/2.5/weather")
-                .addConverterFactory(GsonConverterFactory.create())
-                .client(client)
-                .build();
-
-
-
-        return retrofit;
-    }
 }
